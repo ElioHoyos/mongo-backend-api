3f5c6a7901daa374e19a47ff90ccd676f9107f7c
Elio Hoyos <elio18david@gmail.com>
2025-04-29 19:07:29 -0500

    post/produtos

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -11,4 +11,19 @@ router.get('/',async (req,res) => {
         res.status(500).json({error: e.message});
     }
 });
+//Crear un product
+router.post('/', async (req,res) => {
+    try {
+        const prod = await new Product({
+            nombre:             req.body.nombre,
+            precio:             req.body.precio,
+            categoria_id:       req.body.categoria_id,
+            stock:              req.body.stock,
+            caracteristicas:    req.body.caracteristicas
+        }).save();
+        res.status(201).json(prod);
+    } catch (e) {
+        res.status(400).json({error : e.message});
+    }
+});
 module.exports = router;
